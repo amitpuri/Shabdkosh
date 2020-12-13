6e0ecaba36a32593a7dd0ac9fba05600a7cae8aa
Amit Puri <e.amitpuri@gmail.com>
2020-12-13 16:02:25 +0530

    Update Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -45,9 +45,9 @@ namespace Shabdkosh
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shabdkosh v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shabdkosh v1"));
 
             app.UseRouting();
 
