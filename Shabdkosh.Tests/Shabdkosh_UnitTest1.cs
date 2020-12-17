365ab546220c5cb3934deb1ead75ea5d4e07a902
Amit Puri <amit.puri@in.ibm.com>
2020-12-17 09:08:09 +0530

    separating out test project

---
+++ Shabdkosh.Tests/Shabdkosh_UnitTest1.cs
@@ -0,0 +1,81 @@
+using Shabdkosh.Persistence;
+using Shabdkosh.Services;
+using Shabdkosh.TextOperations;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ShabdKosh.Tests
+{
+    public class Shabdkosh_UnitTest1
+    {
+        private readonly ITextFileRepository fileRepository;
+        private readonly ITextOperation textOperation;
+        private readonly IShabdkoshService shabdkoshService;
+        public Shabdkosh_UnitTest1()
+        {
+            //arrange
+            fileRepository = new TextFileRepository();
+            textOperation = new TextOperation();
+            shabdkoshService = new ShabdkoshService(fileRepository, textOperation);
+        }
+
+        [Fact(DisplayName = "Basic unit test")]
+        [Trait("Category", "unit")]
+        public void Test1_SearchAWord()
+        {
+            //Arrange
+            string keyword = "university";
+            //act
+            var result = shabdkoshService.SearchAWord(keyword);
+            //assert
+            Assert.True(!string.IsNullOrEmpty(result.Definition));
+        }
+
+        [Fact(DisplayName = "Basic unit test")]
+        [Trait("Category", "unit")]
+
+        public void Test2_SearchAWord_NoTFound()
+        {
+            //Arrange
+            string keyword = "hiawatha";
+            //act
+            var result = shabdkoshService.SearchAWord(keyword);
+            //assert
+
+            Assert.True(result.Definition == "[{\"message\":\"No definition :(\"}]");
+        }
+
+        [Fact(DisplayName = "Basic unit test")]
+        [Trait("Category", "unit")]
+        public void Test3_Get_Top5Words()
+        {
+            //arrange
+            var topN = 5;
+            //act
+            var words = shabdkoshService.TopWords(topN);
+            //assert
+            Assert.True(words.Count() == topN);
+        }
+
+        [Fact(DisplayName = "Basic unit test")]
+        [Trait("Category", "unit")]
+        public void Test4_SearchAInvalidWord()
+        {
+            //Arrange
+            string keyword = "test";
+            //act
+            try
+            {
+                shabdkoshService.SearchAWord(keyword);
+                //assert
+                Assert.True(false);
+            }
+            catch (KeyNotFoundException)
+            {
+                //assert
+                Assert.True(true);
+            }
+        }
+    }
+}
